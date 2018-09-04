91856ff2a0ab33d89a928b97eb3d5f9d205c063c
gisselaMauricio <gise.mahu@gmail.com>
2018-09-04 16:32:56 -0500

    first commit

---
+++ src/main/java/com/megacenter/MegaSisApplication.java
@@ -0,0 +1,12 @@
+package com.megacenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaSisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaSisApplication.class, args);
+	}
+}
