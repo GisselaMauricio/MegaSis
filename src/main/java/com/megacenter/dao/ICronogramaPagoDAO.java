de90230e0a3b6ecf28cdbaa345300a0ed48b0c07
gisselaMauricio <gise.mahu@gmail.com>
2018-09-20 17:27:52 -0500

    vv

---
+++ src/main/java/com/megacenter/dao/ICronogramaPagoDAO.java
@@ -0,0 +1,9 @@
+package com.megacenter.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.megacenter.Model.CronogramaPago;
+
+public interface ICronogramaPagoDAO extends JpaRepository<CronogramaPago, Integer>{
+
+}
